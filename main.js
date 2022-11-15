a352b5a0fd0b73c5fef349cd8fd49333c3bf6360
CorujaCorujeira <107893783+CorujaCorujeira@users.noreply.github.com>
2022-11-15 09:07:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+// Create a reference for the 
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //definindo uma variável com uma nova imagem
+	img_imgTag.onload = uploadimg; // ajustando uma função, ao carregar essa variável
+	img_imgTag.src = img_image;   // carregar uma imagem
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Escreva um código para obter o evento key-pressed
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)){
+		alfabetkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or othera alfabet key";
+		console.log("alfabet key")
+    }else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}}
+
+
+
+function aplhabetkey()
+{
+    img_image="alfabeto.png";
+	add();
+}
+function numberkey()
+{
+	img_image="numero.png";
+	add();
+}
+function arrowkey()
+{
+	img_image="drirecional.png";
+	add();
+}
+function specialkey()
+{
+	img_image="special.png";
+	add();
+}
+function otherkey()
+{
+	img_image="outras.png";
+	add();
+}
